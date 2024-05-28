dfb1bf12e6f5259f5596463f68c61d346eeb7bd4
Benjamin Frost <bfrost.schule@gmail.com>
2024-05-28 11:39:41 +0200

    feat: add static events link page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { EventsController } from './events/events.controller';
 import { EventsService } from './events/events.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+      serveRoot: '/static/',
+    }),
+  ],
   controllers: [EventsController],
   providers: [EventsService],
 })
